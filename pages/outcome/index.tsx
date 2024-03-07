82d4b1c73dd127fcf0e58bc4b872d0052eb0a5a9
alfesen <a.fesenko2812@gmail.com>
2024-03-07 10:54:27 +0100

    Create Outcome page

---
+++ pages/outcome/index.tsx
@@ -0,0 +1,9 @@
+import AddOutcome from '@/components/forms/add-outcome.component'
+
+const Outcome = () => {
+  return<main className='container py-4 flex justify-between gap-10'>
+    <AddOutcome className='width=[350]' />
+  </main>
+}
+
+export default Outcome
