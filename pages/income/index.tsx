ee0568af3f158c378e1209ec0e83599bccbfb259
alfesen <a.fesenko2812@gmail.com>
2024-03-07 10:54:09 +0100

    Create Income page

---
+++ pages/income/index.tsx
@@ -0,0 +1,26 @@
+import AddIncome from '@/components/forms/add-income.component'
+import IncomeEntries from '@/components/lists/income-entries.component'
+
+const Income = ({ entries }: { entries: any }) => {
+  return (
+    <main className='container py-4 flex justify-between gap-10'>
+      <AddIncome className='width=[350]' />
+      <IncomeEntries entries={entries} />
+    </main>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3000/api/income', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  })
+  const { data: entries } = await response.json()
+  return {
+    props: {
+      entries: entries,
+    },
+  }
+}
+
+export default Income
